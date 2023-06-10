98f81c0e96b766fee5b231d2fddb9f01053c0451
`Naufal-Pasya` <c038dsx1987@bangkit.academy>
2023-06-10 23:33:28 +0700

    fix forecast

--- api.js
+++ api.js
@@ -28,31 +28,37 @@ admin.initializeApp({
 
 const db = getFirestore();
   
-  api.post('/signup', async (req, res) => {
-    const { email, password, name, address } = req.body;
-  
-      try {
-        // Create a new user using Firebase Admin SDK
-        const userRecord = await admin.auth().createUser({
-          email,
-          password,
-        });
-    
-        // Optional: Add additional user data to Firestore or perform other operations
-        const newUser = await db.collection('users').doc(userRecord.uid).set({
-          email: email || '',
-          name: name || '',
-          address: address || '',
-        });
-  
-        console.log(newUser);
-    
-        res.status(200).json({ message: 'Sign-up successful' });
-      } catch (error) {
-        console.error('Sign-up error:', error);
-        res.status(500).json({ error: 'Sign-up failed' });
-      }
+api.post('/signup', async (req, res) => {
+  const { email, password, name, address } = req.body;
+
+  // Check if any of the required properties are empty
+  if (!email || !password || !name || !address) {
+    return res.status(400).json({ message: 'Missing required fields', error: true });
+  }
+
+  try {
+    // Create a new user using Firebase Admin SDK
+    const userRecord = await admin.auth().createUser({
+      email,
+      password,
     });
+
+    // Optional: Add additional user data to Firestore or perform other operations
+    const newUser = await db.collection('users').doc(userRecord.uid).set({
+      email: email || '',
+      name: name || '',
+      address: address || '',
+    });
+
+    console.log(newUser);
+
+    res.status(200).json({ message: 'Sign-up successful', error: false });
+  } catch (error) {
+    console.error('Sign-up error:', error);
+    res.status(500).json({ error: 'Sign-up failed', error: true });
+  }
+});
+
   
     api.post('/signin', async (req, res) => {
       const { email, password } = req.body;
@@ -61,86 +67,98 @@ const db = getFirestore();
     
         // Verify the provided password
         signInWithEmailAndPassword(auth, email, password)
-        .then(() => {
-          // Sign-in successful
-          const user = auth.currentUser;
-          const { uid } = user;
-          res.status(200).json({ message: 'Sign-in successful', uid });
-        })        
-        .catch((error) => {
-          // Sign-in failed, handle the error
-          console.error('Sign-in error:', error);
-          res.status(401).json({ error: 'Invalid credentials' });
-        });
-    } catch (error) {
-      console.error('Sign-in error:', error);
-      res.status(500).json({ error: 'Sign-in failed' });
-    }
+          .then(() => {
+            // Sign-in successful
+            const user = auth.currentUser;
+            const { uid } = user;
+            res.status(200).json({ message: 'Sign-in successful', uid, error: false });
+          })
+          .catch((error) => {
+            // Sign-in failed, handle the error
+            console.error('Sign-in error:', error);
+            res.status(401).json({ message: 'Invalid credentials', error: true });
+          });
+      } catch (error) {
+        console.error('Sign-in error:', error);
+        res.status(500).json({ message: 'Sign-in failed', error: true });
+      }
     });
   
     api.get('/user', async (req, res) => {
-
       const user = auth.currentUser;
     
       try {
         if (!user) {
-          res.status(401).json({ error: 'User not authenticated' });
+          res.status(401).json({ message: 'User not authenticated', error: true });
         } else {
           const uid = user.uid;
           // Retrieve user data from Firestore
           const userDoc = await db.collection('users').doc(uid).get();
     
           if (!userDoc.exists) {
-            res.status(404).json({ error: 'User not found' });
+            res.status(404).json({ message: 'User not found', error: true });
           } else {
             const userData = userDoc.data();
-            res.status(200).json(userData);
+            res.status(200).json({ ...userData, error: false }); // Add error field as false
           }
         }
       } catch (error) {
         console.error('User data retrieval error:', error);
-        res.status(500).json({ error: 'Failed to retrieve user data' });
+        res.status(500).json({ message: 'Failed to retrieve user data', error: true });
+      }
+    });
+    
+    
+
+    api.get('/weather-forecast', async (req, res) => {
+      // Check if the user is authenticated
+      const user = auth.currentUser;
+      if (!user) {
+        return res.status(401).json({ message: 'User not authenticated', error: true });
+      }
+    
+      const { location } = req.query; // Use req.query to access the query parameters
+    
+      try {
+        // Make a request to WeatherAPI to fetch weather forecast
+        const response = await axios.get('https://api.weatherapi.com/v1/forecast.json', {
+          params: {
+            key: apiKey,
+            q: location
+          }
+        });
+        const data = response.data;
+    
+        // Process the response and extract relevant information
+        const forecastDays = data.forecast.forecastday.map((forecast) => ({
+          date: forecast.date,
+          temperature: forecast.day.avgtemp_c,
+          humidity: forecast.day.avghumidity,
+          windSpeed: forecast.day.maxwind_kph,
+          text: forecast.day.condition.text
+        }));
+    
+        const weatherForecast = {
+          location: data.location.name,
+          forecast: forecastDays
+        };
+    
+        res.status(200).json(weatherForecast);
+      } catch (error) {
+        console.error('Weather retrieval error:', error.data);
+        res.status(500).json({ message: 'Failed to retrieve weather data', error: true });
       }
     });
     
 
-    api.post('/weather-forecast', async (req, res) => {
-        // Check if the user is authenticated
-        const user = auth.currentUser;
-        if (!user) {
-          return res.status(401).json({ error: 'User not authenticated' });
-        }
-      
-        const { location } = req.body;
-      
+      api.post('/signout', async (req, res) => {
         try {
-          // Make a request to WeatherAPI to fetch weather forecast
-          const response = await axios.get('https://api.weatherapi.com/v1/forecast.json', {
-            params: {
-              key: apiKey,
-              q: location
-            }
-          });
-          const data = response.data;
-      
-          // Process the response and extract relevant information
-          const forecastDays = data.forecast.forecastday.map((forecast) => ({
-            date: forecast.date,
-            temperature: forecast.day.avgtemp_c,
-            humidity: forecast.day.avghumidity,
-            windSpeed: forecast.day.maxwind_kph,
-            text: forecast.day.condition.text
-          }));
-      
-          const weatherForecast = {
-            location: data.location.name,
-            forecast: forecastDays
-          };
-      
-          res.status(200).json(weatherForecast);
+          // Sign out the currently authenticated user
+          await auth.signOut();
+          res.status(200).json({ message: 'Sign-out successful', error: false });
         } catch (error) {
-          console.error('Weather retrieval error:', error);
-          res.status(500).json({ error: 'Failed to retrieve weather data' });
+          console.error('Sign-out error:', error);
+          res.status(500).json({ error: 'Sign-out failed', error: true });
         }
       });
   
